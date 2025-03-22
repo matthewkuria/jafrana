c61fd10bf3d133caabc1bf1bc7250e8df40124cc
Matthewkuria <mathewkwachira@gmail.com>
2025-03-22 22:37:02 +0300

    Re-edit on the hero section

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,17 +1,27 @@
 import React from 'react'
 import Image from 'next/image'
 import heroImage from '../../public/hero-image.jpeg'
+import { bebas_neue } from '../fonts/fonts'
 const HeroSection = () => {
     return (
-    <div className="grid grid-cols-1 md:grid-cols-2 gap-0 md:gap-4">
-         <div className="flex flex-col items-center justify-center gap-3  p-2">
-            <h1 className="text-xl md:text-4xl font-semibold text-primary">Jafrana Construction company</h1>
-            <p className="text-lg text-secondary">We build your dream home</p>
-            <button className="bg-primary text-white px-4 py-2 rounded-lg">Get Started</button> 
+    <div className=" flex flex-col-reverse md:flex md:flex-row md:justify-between  text-[13px] md:text-[16px] p-3 md:p-8">
+            <div className="flex flex-col justify-end   p-4 md:max-w-1/2">
+            <p className="text-secondary">Welcome to</p>
+            <h1 className="flex flex-col text-xl md:text-4xl font-bold text-primary">Jafrana Construction Company</h1>
+                <p className={`${bebas_neue.className} text-2xl`}>We build your dream home</p>
+                <p className="text-secondary text-xs">We are experts who specializes in helping 
+                    individuals and businesses to build their dream homes and offices.
+                </p>
+                <div className="my-4">
+                    <button className="bg-primary text-white px-4 py-2 rounded-lg hover:outline-2 hover:text-primary hover:bg-quaternary hover:outline-solid">
+                        Get Started
+                    </button>
+                </div> 
         </div>
-            <div className="flex flex-col items-center justify-center">
+            <div className="flex flex-col items-center justify-center p-2 relative ">
+                <div className="absolute bg-primary w-[90px] h-[90px] rounded-full -top-10 -left-10"></div>
                 <Image src={heroImage} alt="hero" width={500} height={500}
-                className='h-[350px] shadow-primary rounded-md'/>
+                className='h-[350px] shadow-primary shadow-md rounded-md'/>
             </div>
     </div>
   )
---
+++ app/fonts/fonts.ts
@@ -0,0 +1,36 @@
+import { Red_Hat_Text, Modak, Inter, Roboto_Mono, New_Rocker, Bebas_Neue, Playfair_Display } from "next/font/google";
+
+export const redHatText = Red_Hat_Text({
+    subsets: ['latin'],
+    display: 'swap',
+})
+export const playfairDisplay = Playfair_Display({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
+export const bebas_neue = Bebas_Neue({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
+export const new_rocker = New_Rocker({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
+export const modak = Modak({
+    subsets: ['latin'],
+    display: 'swap',
+    weight: "400"
+})
+
+export const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
+ 
+export const roboto_mono = Roboto_Mono({
+  subsets: ['latin'],
+  display: 'swap',
+})

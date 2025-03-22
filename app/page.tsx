99c69788ba39984942594cca9580ef3fc987dcaf
Matthewkuria <mathewkwachira@gmail.com>
2025-03-22 23:01:04 +0300

    Add some margin

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import HeroSection from "./components/HeroSection";
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen py-2">
-      <section className="">
+      <section className="p-2">
         <HeroSection />
      </section>
     </main>

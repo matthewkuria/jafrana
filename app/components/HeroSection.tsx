95cc5756a488946e7bd6aef8a579c8c98510e04a
Matthewkuria <mathewkwachira@gmail.com>
2025-03-22 23:06:59 +0300

    Hotfix the background for the picture

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -4,7 +4,7 @@ import heroImage from '../../public/hero-image.jpeg'
 import { bebas_neue } from '../fonts/fonts'
 const HeroSection = () => {
     return (
-    <div className=" flex flex-col-reverse md:flex md:flex-row md:justify-between  text-[13px] md:text-[16px] p-3 md:p-8 mt-10">
+    <div className=" flex flex-col-reverse md:flex md:flex-row md:justify-between  text-[13px] md:text-[16px] p-3 md:p-8 mt-16">
             <div className="flex flex-col justify-end   p-4 md:max-w-1/2">
             <p className="text-secondary text-xs">Welcome to</p>
             <h1 className="flex flex-col text-xl md:text-4xl font-bold text-primary">Jafrana Construction Company</h1>
@@ -19,11 +19,10 @@ const HeroSection = () => {
                 </div> 
         </div>
             <div className="flex flex-col items-center justify-center p-2 relative m-1 ">
-                {/* <div className="absolute bg-primary w-[50px] h-[50px] rounded-full -top-5 -left-5 z-30"></div> */}
-                 <div className="absolute bg-primary w-[300px] h-[50px]  -top-5 -left-5 z-0"></div>
+                 <div className="absolute bg-primary w-[280px] h-[42px]  -top-2 -left-5 z-0"></div>
                 <Image src={heroImage} alt="hero" width={500} height={500}
                     className='h-[350px] shadow-primary shadow-md z-20 ' />
-                <div className="absolute bg-primary w-[300px] h-[30px]  -bottom-3 -right-3 z-0"></div>
+                <div className="absolute bg-primary w-[280px] h-[30px]  -bottom-3 -right-3 z-0"></div>
             </div>
     </div>
   )

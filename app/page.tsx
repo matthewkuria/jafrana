2ec04a5c8e874152d9499f537ea4d31909a5cae2
Matthewkuria <mathewkwachira@gmail.com>
2025-03-22 20:24:53 +0300

    Initial App setup

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen py-2">
+      <h1 className="text-4xl font-semibold text-primary">Jafrana Construction company</h1>
+    </main>
+  );
+}

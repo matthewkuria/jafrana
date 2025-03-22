483941cd25777006b6d6ac4d2b1f2430c4a9e6aa
Matthewkuria <mathewkwachira@gmail.com>
2025-03-23 00:22:32 +0300

    Add the number of the experience on the who we are.

--- app/components/HeroSection.tsx
+++ app/components/HeroSection.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Image from 'next/image'
-import heroImage from '../../public/home (1).jpeg'
+import heroImage from '../../public/hero-image.jpeg'
 import { bebas_neue } from '../fonts/fonts'
 const HeroSection = () => {
     return (
@@ -8,20 +8,20 @@ const HeroSection = () => {
             <div className="flex flex-col justify-end   p-4 md:max-w-1/2 mt-5">
             <p className="text-secondary">Welcome to</p>
             <h1 className="flex flex-col text-xl md:text-4xl font-bold text-primary">Jafrana Construction Company</h1>
-                <p className={`${bebas_neue.className} text-2xl`}>We build your dream home</p>
+                <p className={`${bebas_neue.className} text-2xl my-1`}>We build your dream home</p>
                 <p className="text-secondary ">We are experts who specializes in helping 
                     individuals and businesses to build their dream homes and offices.
                 </p>
                 <div className="my-4">
                     <button className="bg-primary text-white px-4 py-2 hover:outline-2 hover:text-primary hover:bg-quaternary hover:outline-solid">
-                        Get Started
+                        Get In Touch
                     </button>
                 </div> 
         </div>
             <div className="flex flex-col items-center justify-center p-2 relative m-1 ">
                  <div className="absolute bg-primary w-[280px] h-[42px]  -top-2 -left-3 z-0"></div>
                 <Image src={heroImage} alt="hero" width={500} height={500}
-                    className='h-[350px] shadow-primary shadow-md z-20'/>
+                    className='h-[250px] md:h-[350px]  shadow-primary shadow-md z-20'/>
                 <div className="absolute bg-primary w-[280px] h-[30px]  -bottom-3 -right-3 z-0"></div>
             </div>
     </div>
--- app/components/WhoWeAre.tsx
+++ app/components/WhoWeAre.tsx
@@ -1,23 +1,33 @@
 import React from 'react'
 import Image from 'next/image'
 import heroImage from '../../public/home (8).jpeg'
-import { bebas_neue } from '../fonts/fonts'
+import { bebas_neue, redHatText } from '../fonts/fonts'
 const WhoWeAre = () => {
     return (
     <div className=" flex flex-col-reverse md:flex md:flex-row md:justify-between  text-[13px] md:text-[16px] p-3 md:p-8">
             <div className="flex flex-col items-center justify-center p-2 relative m-1 ">
-                 <div className="absolute bg-primary w-[280px] h-[42px]  -top-2 -left-3 z-0"></div>
+                <div className="absolute bg-primary w-[280px] h-[42px]  -top-2 -left-3 z-0"></div>
                 <Image src={heroImage} alt="hero" width={500} height={500}
-                    className='h-[350px] shadow-primary shadow-md z-20'/>
+                    className='h-[300px] md:h-[350px] shadow-primary shadow-md z-20'/>
                 <div className="absolute bg-primary w-[280px] h-[30px]  -bottom-3 -right-3 z-0"></div>
+                <div className="absolute bg-tertiary w-[200px] h-[150px]  -bottom-3 -right-3 z-20 flex flex-col items-center justify-center">
+                    <p className="text-primary text-3xl font-bold">10 <span className="text-quaternary text-base">+ years</span></p>
+                    <p className="text-quaternary">Experience</p>
+                </div>
             </div>
-            <div className="flex flex-col justify-end   p-4 md:max-w-1/2 mt-5">
-                <p className="text-secondary text-xs">Welcome to</p>
-                <h1 className="flex flex-col text-xl md:text-4xl font-bold text-primary">Jafrana Construction Company</h1>
-                <p className={`${bebas_neue.className} text-2xl`}>We build your dream home</p>
-                <p className="text-secondary text-xs">We are experts who specializes in helping 
-                    individuals and businesses to build their dream homes and offices.
+            <div className="flex flex-col justify-end  items-start   p-4 md:max-w-1/2 mt-5">
+                <p className={`${bebas_neue.className} text-2xl text-primary`}>Who We Are</p>
+                <p className={`${redHatText.className} font-bold`}>We Are The Best Construction Company</p>
+                <p className="text-secondary">
+                    At Jafrana Construction Company,
+                    we are passionate about turning your visions into reality.
+                    With years of experience in the construction industry,
+                    we pride ourselves on delivering high-quality craftsmanship,
+                    innovative designs, and exceptional customer service. Whether
+                    it's building your dream home or creating a functional office space,
+                    we are committed to excellence in every project we undertake.
                 </p>
+                
                 <div className="my-4">
                     <button className="bg-primary text-white px-4 py-2  hover:outline-2 hover:text-primary hover:bg-quaternary hover:outline-solid">
                         Discover More

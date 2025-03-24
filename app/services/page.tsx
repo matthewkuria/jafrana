013ff2ca0c6fad7b608a1b07e54e276e810a9b65
Matthewkuria <mathewkwachira@gmail.com>
2025-03-24 19:37:28 +0300

    Add services cards to the services page

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import Image from 'next/image'
 import { bebas_neue } from '../fonts/fonts'
+import construction from '../../public/home (1).jpeg'
 
 const page = () => {
   return (
-    <section className="services min-h-screen flex  flex-col items-center justify-center">
-          <h1 className={`${bebas_neue.className} text-xl`}>Services</h1>
+    <section className="services min-h-screen flex  flex-col items-center justify-center w-full">
+      <h1 className={`${bebas_neue.className} text-3xl text-quaternary underline decoration-primary`}>Services</h1>
+      <article className='bg-quaternary grid grid-cols-1 md:grid-cols-3 gap-5 p-5 w-full md:w-3/4'>
+        <div className="service-card">
+          <Image src={construction} alt="construction" width={200} height={200}
+            className='rounded-md'
+          />
+            <p className="title">Construction</p>
+        </div>
+        <div className="service-card">
+          <Image src={construction} alt="construction" width={200} height={200}
+            className='rounded-md'
+          />
+            <p className="title">Renovation</p>
+        </div>
+        <div className="service-card">
+          <Image src={construction} alt="construction" width={200} height={200}
+            className='rounded-md'
+          />
+            <p className="title">Interior Design</p>
+        </div>
+      </article>
     </section>
   )
 }

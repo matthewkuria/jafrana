a51654fe4754f291709d8eb8bc2595d11b2b888e
Matthewkuria <mathewkwachira@gmail.com>
2025-03-24 19:01:31 +0300

    Add services page and its styling

---
+++ app/services/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { bebas_neue } from '../fonts/fonts'
+
+const page = () => {
+  return (
+    <section className="services min-h-screen flex  flex-col items-center justify-center">
+          <h1 className={`${bebas_neue.className} text-xl`}>Services</h1>
+    </section>
+  )
+}
+
+export default page

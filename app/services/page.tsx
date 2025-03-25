37224ec02a0c60a6421fb33fe1c75d3327c3d638
Matthewkuria <mathewkwachira@gmail.com>
2025-03-25 16:45:51 +0300

    set the flex properties to the the service cards

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -2,29 +2,30 @@ import React from 'react'
 import Image from 'next/image'
 import { bebas_neue } from '../fonts/fonts'
 import construction from '../../public/home (1).jpeg'
+import renovation from "../../public/home (6).jpeg"
 
 const page = () => {
   return (
     <main className="services  flex  flex-col items-center justify-center w-full">
       <h1 className={`${bebas_neue.className} text-3xl text-primary underline decoration-primary`}>Services</h1>
-      <article className='bg-quaternary flex flex-col items-center justify-around w-full'>
+      <article className='bg-quaternary flex flex-col md:flex md:flex-row items-center justify-around w-full'>
         <div className="service-card">
           <Image src={construction} alt="construction" width={200} height={200}
-            className='rounded-md'
+            className='rounded-md h-[200px] w-[300px] service-card-image'
           />
           <p className="title">Construction</p>
           <p className="">We offer a wide range of construction services to meet your needs.</p>
         </div>
         <div className="service-card">
-          <Image src={construction} alt="construction" width={200} height={200}
-            className='rounded-md'
+          <Image src={renovation} alt="construction" width={200} height={200}
+            className='rounded-md h-[200px] w-[300px] service-card-image'
           />
           <p className="title">Renovation</p>
           <p className="">We offer a wide range of renovation services to meet your needs.</p>
         </div>
         <div className="service-card">
           <Image src={construction} alt="construction" width={200} height={200}
-            className='rounded-md'
+            className='rounded-md h-[200px] w-[300px] service-card-image'
           />
           <p className="title">Interior Design</p>
           <p className="">We offer a wide range of interior design services to meet your needs.</p>

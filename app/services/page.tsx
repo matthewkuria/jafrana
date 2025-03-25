6931ac22f60ab7e7d00c62bdce06ebda5525d7ee
Matthewkuria <mathewkwachira@gmail.com>
2025-03-25 16:07:24 +0300

    Set the services section to 100vh min-height

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -5,9 +5,9 @@ import construction from '../../public/home (1).jpeg'
 
 const page = () => {
   return (
-    <section className="services min-h-screen flex  flex-col items-center justify-center w-full">
+    <main className="services  flex  flex-col items-center justify-center w-full">
       <h1 className={`${bebas_neue.className} text-3xl text-primary underline decoration-primary`}>Services</h1>
-      <article className='bg-quaternary flex items-center justify-around w-full'>
+      <article className='bg-quaternary flex flex-col items-center justify-around w-full'>
         <div className="service-card">
           <Image src={construction} alt="construction" width={200} height={200}
             className='rounded-md'
@@ -30,7 +30,7 @@ const page = () => {
           <p className="">We offer a wide range of interior design services to meet your needs.</p>
         </div>
       </article>
-    </section>
+    </main>
   )
 }
 

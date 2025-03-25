7ae79a27926a058ab42d22315701cefd54dbbb9e
Matthewkuria <mathewkwachira@gmail.com>
2025-03-25 15:57:16 +0300

    change the services card from grids to flex

--- app/services/page.tsx
+++ app/services/page.tsx
@@ -6,8 +6,8 @@ import construction from '../../public/home (1).jpeg'
 const page = () => {
   return (
     <section className="services min-h-screen flex  flex-col items-center justify-center w-full">
-      <h1 className={`${bebas_neue.className} text-3xl text-quaternary underline decoration-primary`}>Services</h1>
-      <article className='bg-quaternary grid grid-cols-1 md:grid-cols-3 gap-5 p-5 w-full md:w-3/4'>
+      <h1 className={`${bebas_neue.className} text-3xl text-primary underline decoration-primary`}>Services</h1>
+      <article className='bg-quaternary flex items-center justify-around w-full'>
         <div className="service-card">
           <Image src={construction} alt="construction" width={200} height={200}
             className='rounded-md'

90cdbc8d334cdb6ca98fc1daf15d84e43bc77652
Matthewkuria <mathewkwachira@gmail.com>
2025-03-29 23:22:18 +0300

    Set the who we  are image to 300px for desktop design

--- app/components/WhoWeAre.tsx
+++ app/components/WhoWeAre.tsx
@@ -5,12 +5,13 @@ import { bebas_neue, redHatText } from '../fonts/fonts'
 import Link from 'next/link'
 const WhoWeAre = () => {
     return (
-        <section className=" flex flex-col-reverse md:flex md:flex-row md:justify-between  text-[13px] md:text-[16px] p-3 md:p-8">
-            <div className="">
+        <section className="flex flex-col items-center justify-center bg-white text-primary p-5 md:p-10">
+            <h3 className="text-tertiary text-2xl font-bold">About Us</h3>
+            <div className=" flex flex-col-reverse md:flex md:flex-row md:justify-between  text-[13px] md:text-[16px] p-3 ">
                 <div className="flex flex-col items-center justify-center p-2 relative m-1 ">
                     <div className="absolute bg-primary w-[280px] h-[42px]  -top-2 -left-3 z-0"></div>
-                    <Image src={heroImage} alt="hero" width={500} height={500}
-                        className='h-[300px] md:h-[350px] shadow-primary shadow-md z-20'/>
+                    <Image src={heroImage} alt="hero" width={400} height={200}
+                        className='h-[300px] md:h-[300px] shadow-primary shadow-md z-20'/>
                     <div className="absolute bg-primary w-[280px] h-[30px]  -bottom-3 -right-3 z-0"></div>
                     <div className="absolute bg-tertiary w-[120px] h-[100px]  -bottom-3 -right-3 z-20 flex flex-col items-center justify-center">
                         <p className="text-primary text-xl md:text-3xl font-bold">10 <span className="text-quaternary text-base">+ years</span></p>
@@ -19,7 +20,7 @@ const WhoWeAre = () => {
                 </div>
             <div className="flex flex-col justify-start  items-start  md:max-w-1/2 mt-5">
                 <p className={`${bebas_neue.className} text-2xl text-primary`}>Who We Are</p>
-                <p className={`${redHatText.className} font-bold`}>We Are The Best Construction Company</p>
+                <p className="text-black font-semibold">We Are The Best Construction Company</p>
                 <p className="text-secondary">
                     At Jafrana Construction Company,
                     we are passionate about turning your visions into reality.
@@ -37,20 +38,14 @@ const WhoWeAre = () => {
                 </div> 
             </div>
             </div>
-            <article className=''>
-                <div className="">
+            <article className="flex flex-col md:flex-row items-center justify-center md:justify-between bg-white text-primary p-5">
+                <div className="flex flex-col justify-start  items-start  md:max-w-1/2 mt-5">
                     <p className="">Why Choose Us</p>
                     <p className={`${redHatText.className} font-bold`}>We Are The Best Construction Company</p>
                 </div>
-                <div className="flex flex-col items-center justify-center p-2 relative m-1 ">
-                    <div className="absolute bg-primary w-[280px] h-[42px]  -top-2 -left-3 z-0"></div>
-                    <Image src={heroImage} alt="hero" width={500} height={500}
-                        className='h-[300px] md:h-[350px] shadow-primary shadow-md z-20'/>
-                    <div className="absolute bg-primary w-[280px] h-[30px]  -bottom-3 -right-3 z-0"></div>
-                    <div className="absolute bg-tertiary w-[120px] h-[100px]  -bottom-3 -right-3 z-20 flex flex-col items-center justify-center">
-                        <p className="text-primary text-xl md:text-3xl font-bold">10 <span className="text-quaternary text-base">+ years</span></p>
-                        <p className="text-quaternary">Experience</p>
-                    </div>
+                <div className="flex flex-col items-center justify-center p-2 relative m-1 md:max-w-1/2">
+                    <Image src={heroImage} alt="hero" width={800} height={500}
+                        className='h-[300px] md:h-[300px]  z-20'/>
                 </div>
                 
             </article>

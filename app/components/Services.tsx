ba0b238391bce1b4aee68473c5d30bb41d50bc00
Matthewkuria <mathewkwachira@gmail.com>
2025-04-03 23:02:21 +0300

    Add margin left for the services

--- app/components/Services.tsx
+++ app/components/Services.tsx
@@ -8,7 +8,7 @@ const Services = () => {
             <p className="text-center text-tertiary">We offer a wide range of services to meet your needs</p>
           </div>
           <div className="">
-              <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5 md:p-10">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-5 md:p-10 md:ml-30">
                     <div className="">
                         <h2 className="text-secondary text-2xl font-bold">Construction</h2>
                         <p className="text-tertiary">We offer a wide range of construction services to meet your needs</p>

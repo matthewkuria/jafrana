aacd719e86a208e75a45443058422c1895158cab
Matthewkuria <mathewkwachira@gmail.com>
2025-04-11 20:53:33 +0300

    Add the new testimonials

--- app/components/Testimonials.tsx
+++ app/components/Testimonials.tsx
@@ -8,14 +8,14 @@ const testimonials = [
     name: 'susan smith',
     job: 'web developer',
     img: 'https://www.course-api.com/images/people/person-1.jpeg',
-    feedback: "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    feedback: " I took the construction services from Jafrana Construction Company recently and was amazed by their quality craftsmanship. They finished the work within the given timeframe without compromising quality.",
   },
   {
     id: 2,
     name: 'anna johnson',
     job: 'web designer',
     img: 'https://www.course-api.com/images/people/person-2.jpeg',
-    feedback: 'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    feedback: 'If someone is searching for the best construction company for custom home building. Trust Jafrana Construction Company. They were really very easy to work with.',
   },
   {
     id: 3,

85192abb35ab0450c139213f738bd5df430ba9e2
Matthewkuria <mathewkwachira@gmail.com>
2025-04-12 01:39:41 +0300

    add background color to the whatsapp text

--- app/components/WhatsappIcon.tsx
+++ app/components/WhatsappIcon.tsx
@@ -9,7 +9,7 @@ const WhatsappIcon = () => {
               <Link href="https://api.whatsapp.com/send?phone=254722553222&text=Hello%20Jafrana%20Construction%20Company%2C%20I%20would%20like%20to%20inquire%20about%20your%20services." target="_blank" rel="noopener noreferrer"
               className="flex items-center"
               >
-                <p className="text-[11px]">Need help? <span className="font-bold ">Chat with us</span> </p>
+                <p className="text-[11px] bg-slate-100 px-1 rounded-md">Need help? <span className="font-bold ">Chat with us</span> </p>
                 <Image alt='Whatsapp icon' src={whatsappIcon} height={60} width={60}
                 className="animate-pulse" />
               </Link>

98b65f6125cd0d1b2c100c976f52a48f629cfb77
Matthewkuria <mathewkwachira@gmail.com>
2025-04-11 23:33:45 +0300

    For the large screens set the padding bottom to 4

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -5,7 +5,7 @@ const page = () => {
   return (
     <main className=" flex flex-col p-1">
       <section className="flex flex-col items-center justify-center min-h-[300px] md:min-h-[450px] lg:min-h-[490px] projects">
-          <div className="bg-slate-900  py-1 px-4 md:px-4 lg:px-6 lg:py-2 ">
+          <div className="bg-slate-900  py-1 px-4 md:px-4 lg:px-6 lg:pt-3 lg:pb-4">
             <p className={`${playfairDisplay.className} text-secondary text-3xl md:text-4xl lg:text-5xl `}>Our Projects</p>
       </div>      
     </section>

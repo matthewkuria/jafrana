e002446a8d1afbc681861314674ebe1043dbc3d2
Matthewkuria <mathewkwachira@gmail.com>
2025-04-11 23:31:13 +0300

    Replace the bebas_neue font with the playFairDisplay for the projects title

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react'
-import { bebas_neue } from '../fonts/fonts'
+import { playfairDisplay } from '../fonts/fonts'
 
 const page = () => {
   return (
-    <main className="flex flex-col items-center justify-center min-h-[300px] md:min-h-[450px] lg:min-h-[490px] projects">
-          <div className="  py-1 px-4 md:px-4 lg:px-6 lg:py-2  ">
-            <p className={`${bebas_neue.className} text-secondary text-3xl md:text-4xl lg:text-5xl `}>Projects</p>
-          </div>
+    <main className=" flex flex-col p-1">
+      <section className="flex flex-col items-center justify-center min-h-[300px] md:min-h-[450px] lg:min-h-[490px] projects">
+          <div className="bg-slate-900  py-1 px-4 md:px-4 lg:px-6 lg:py-2 ">
+            <p className={`${playfairDisplay.className} text-secondary text-3xl md:text-4xl lg:text-5xl `}>Our Projects</p>
+      </div>      
+    </section>
+    <section className="flex flex-col items-center justify-center w-3/4 md:w-1/2 mx-auto mt-10 mb-10 text-center"> 
+        <p className="">
+          At Jafrana Construction Company we provide a comprehensive building service which is delivered through our experienced teams meeting the needs of all clients operating across a wide range of markets.
+        </p>
+      </section>
     </main>
   )
 }

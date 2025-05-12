5d54e6a858404bd268a5711fd09abce97e1c92e1
Matthewkuria <mathewkwachira@gmail.com>
2025-05-12 23:05:52 +0300

    Remove unused images in the gallery page

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -1,15 +1,9 @@
 "use client";
 import React, { useState } from 'react'
 import Gallery from '../components/Gallery'
-import Image from 'next/image'
-import mainImage from "../../public/hero-image.jpeg"
-import image1 from "../../public/home (1).jpeg"
-import image2 from "../../public/home (2).jpeg"
-import image3 from "../../public/home (3).jpeg"
-import image4 from "../../public/home (4).jpeg"
+
 
 const Page = () => {
-  const [featuredImage, setFeaturedImage] = useState(mainImage)
 
   return (
     <main className="flex flex-col items-center justify-center p-4 bg-gray-100">
